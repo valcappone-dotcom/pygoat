6ca4328a6119b299c753a4871bd55377f41236ff
valcappone-dotcom <valcappone@googlemail.com>
2026-09-04 11:38:38 +0200

    Update settings.py

--- pygoat/settings.py
+++ pygoat/settings.py
@@ -169,4 +169,4 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 SECRET_COOKIE_KEY = "PYGOAT"
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000","http://0.0.0.0:8000","http://172.16.189.10","https://*.azurewebsites.net"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000","http://0.0.0.0:8000","http://172.16.189.10","https://mywebapptest-d4c8a0bwdygbdmav.westus3-01.azurewebsites.net"]
